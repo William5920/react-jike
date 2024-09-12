ca308c07de88f215d6200ce609e9ce82e72941ae
unknown <1101044179@qq.com>
2024-09-12 17:28:34 +0800

    完成基础路由配置

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import router from './router'
+import {RouterProvider} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
