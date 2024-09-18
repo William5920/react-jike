22077776cda40f20aeda22a053a8ca58fe6b48bb
unknown <1101044179@qq.com>
2024-09-18 10:52:50 +0800

    完成登录静态页

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+// import App from './App';
 import router from './router'
 import {RouterProvider} from 'react-router-dom'
 
