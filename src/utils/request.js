28d97e2b907fb1b03ed2d0f62522cd778ceb8ce7
unknown <1101044179@qq.com>
2024-09-19 19:45:43 +0800

    请求拦截器注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {getToken} from '@/utils'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -7,6 +8,10 @@ const request = axios.create({
 
 // 添加请求拦截
 request.interceptors.request.use(config => {
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bear ${token}`
+  }
   return config
 }, error => {
   return Promise.reject(error)
